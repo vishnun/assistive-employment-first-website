40328766426adb9b503e6db1a36628aaf539981b
Vishnu <vishnunarang2000@gmail.com>
2018-02-09 11:27:40 -0700

    Setting up base project. JS task pending.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # assistive-employment-first-website
 Making employment first website and also making it assistive for people with certain disabilities to be able to effectively use the website and find useful information.
+
+## Setup instructions
+
+- **Install Node.js**
+
+- **Install gulp**  (To learn more about Gulp: see [gulp-tutorial][a17eda96])  
+`sudo npm install gulp -g`
+
+
+
+
+### Only one Time needed when setting up by the first team member. Others don't need this.
+
+- create a gulp project from within the project directory.  
+  `npm init`  
+  Answer all the prompts to setup the `package.json` file.
+
+- Once package json is created, we can setup gulp into the project.  
+  `npm install gulp --save-dev`
+
+- Install sass pre processing for gulp  
+  `npm install gulp-sass --save-dev`
+
+
+
+  [a17eda96]: https://css-tricks.com/gulp-for-beginners/ "gulp tutorial"
---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+// Node modules
+var fs = require('fs'),
+  vm = require('vm'),
+  merge = require('deeply'),
+  chalk = require('chalk'),
+  es = require('event-stream'),
+  browserSync = require('browser-sync').create();;
+
+// Gulp and plugins
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  clean = require('gulp-clean'),
+  replace = require('gulp-replace'),
+  uglify = require('gulp-uglify'),
+  htmlreplace = require('gulp-html-replace'),
+  sass = require('gulp-sass');
+
+// Discovers all AMD dependencies, concatenates together all required .js files, minifies them
+// gulp.task('js', function() {
+//   return rjs(requireJsOptimizerConfig)
+//     .pipe(uglify({
+//       preserveComments: 'some'
+//     }))
+//     .pipe(gulp.dest('./dist/'));
+// });
+
+gulp.task('images', function() {
+  return es.concat(gulp.src('app/images/*')
+    .pipe(gulp.dest("dist/images")),
+    gulp.src('app/favicon.png')
+    .pipe(gulp.dest("dist"))
+  );
+});
+
+// Transpiles sass -> css and minifies, rewrites relative paths to Bootstrap fonts, copies Bootstrap fonts
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass({
+      compress: true
+    }))
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+// Moves the bootstrap fonts to the dist-folder
+gulp.task('fonts', function() {
+  return es.concat(gulp.src('./node_modules/bootstrap/fonts/*', {
+      base: './node_modules/bootstrap/components-bootstrap/'
+    })
+    .pipe(gulp.dest('./dist/fonts')),
+    gulp.src('./node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('./dist/fonts')));
+});
+
+gulp.task('devsass', function() {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('app/scss/**/*.scss', ['devsass']);
+});
+
+
+// Copies index.html, replacing <script> and <link> tags to reference production URLs
+gulp.task('html', function() {
+  return gulp.src('app/index.html')
+    .pipe(htmlreplace({
+      'css': 'css.css',
+      'js': 'scripts.js'
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+// Removes all files from ./dist/
+gulp.task('clean', function() {
+  return gulp.src('dist/**/*', {
+      read: false
+    })
+    .pipe(clean());
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+gulp.task('default', ['html', 'js', 'sass', 'fonts'], function(callback) {
+  callback();
+  console.log('\nPlaced optimized files in ' + chalk.magenta('dist/\n'));
+});
