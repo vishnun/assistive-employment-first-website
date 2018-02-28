c12f601c4049577fe0b5e67fbc12892a45a5e6f4
Vishnu <vishnunarang2000@gmail.com>
2018-02-28 16:24:43 -0700

    home page get started action

--- app/js/home-page.js
+++ app/js/home-page.js
@@ -2,4 +2,11 @@ $(function() {
   if($('#home-page').length == 0) {
     return;
   }
+
+  $('.role-selection').hide();
+
+  $('#get-started').on('click', function() {
+    $(this).hide();
+    $('.role-selection').show();
+  });
 });
