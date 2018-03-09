5c48104c7df23974dfbc6aaf3e2375fd11761cfe
Vishnu Narang <vishnunarang2000@gmail.com>
2018-03-09 00:12:51 -0700

    You can zoom in and out text size for the website using assistive support tool

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,41 @@
 $(function () {
+  bindCalendarButtons();
+  bindAssistiveSupportButtons();
+});
+
+function bindCalendarButtons() {
   $('.expand-calendar').on('click', function () {
-    $('.ef-calendar').addClass('show');
+    $('.ef-calendar').toggleClass('show');
   });
   
   $('.close-button').on('click', function () {
-    $('.ef-calendar').removeClass('show');
+    $('.ef-calendar').toggleClass('show');
   });
+  
+}
+
+
+function bindAssistiveSupportButtons() {
+  var $rootEl = $('html');
   
   $('.assistive-support').on('click', function () {
     $('.ef-assistive-drawer').toggleClass('show');
   });
   
-});
+  $('#zoom-in-text').on('click', function () {
+    var currentFontSize = $rootEl.css('font-size');
+    if (parseInt(currentFontSize) === 20) {
+      return;
+    }
+    $rootEl.css('font-size', parseInt(currentFontSize) + 1 + 'px');
+  });
+  
+  $('#zoom-out-text').on('click', function () {
+    var currentFontSize = $rootEl.css('font-size');
+    if (parseInt(currentFontSize) === 12) {
+      return;
+    }
+    $rootEl.css('font-size', parseInt(currentFontSize) - 1 + 'px');
+  });
+  
+}
