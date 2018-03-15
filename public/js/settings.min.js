7fdb9bbe256601b9b41ad7f480d534add21dadac
Vishnu Narang <vishnunarang2000@gmail.com>
2018-03-15 14:01:30 -0700

    Adding a settings js.

---
+++ public/js/settings.min.js
@@ -0,0 +1,39 @@
+function localStorageExists() {
+	if (typeof(Storage) !== "undefined") {
+		return true;
+	}
+	return false;
+}
+
+var Settings = {
+	updateFontSize: function(fontSize) {
+		if (localStorageExists()) {
+			localStorage.setItem('fontSize', fontSize);
+		}
+	}
+}
+
+function updateFontSize() {
+	var rootEl = document.getElementsByTagName('html')[0];
+	var fontSize = localStorage.getItem('fontSize');
+	if (fontSize) {
+		rootEl.style.fontSize = fontSize;
+	}
+}
+
+
+function ready(fn) {
+	if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+		fn();
+	} else {
+		document.addEventListener('DOMContentLoaded', fn);
+	}
+}
+
+ready(function() {
+	if (!localStorageExists()) {
+		return;
+	}
+
+	updateFontSize();
+});
